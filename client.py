1a42084370db92cc6515744291b0cf56b858802e
curringtpm17 <33531586+curringtpm17@users.noreply.github.com>
2018-04-05 21:06:13 -0500

    updated client

--- client.py
+++ client.py
@@ -8,20 +8,32 @@ clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
 running = False
 
+def chatRoom(clientSocket, x):
+    global running
+    running = True
+
+    while running:
+        msg = clientSocket.recv(1024).decode('ascii')
+        if len(msg) > 0:
+            print(msg)
+        elif msg.upper() == "QUIT CHATROOM":
+            break
+
 while True:
     sentence = input("Input lowercase sentence:")#hello world
     #if not sentence:
      #   break
+    x = "hello"
     clientSocket.send(sentence.encode())
     modifiedSentence = clientSocket.recv(1024).decode('ascii')
     print("From Server:", modifiedSentence)
 
-    if "Connected" is in modifiedSentence:
-        start_new_thread(chatRoom(clientSocket))
+    if "Connected" in modifiedSentence:
+        start_new_thread(chatRoom, (clientSocket, x))
         while True:#chatroom while loop
             msg = input("Enter your message: ")
             clientSocket.send(msg.encode())
-            if msg.upper() == "EXITING CHATROOM":
+            if msg.upper() == "QUIT CHATROOM":
                 break
             
     #receiving = clientSocket.recv(1024).decode('ascii')
@@ -32,15 +44,6 @@ while True:
     
 clientSocket.close()
 
-def chatRoom(clientSocket):
-    global running
-    running = True
 
-    while running:
-        msg = clientSocket.recv(1024).decode('ascii')
-        if len(msg) > 0:
-            print(msg)
-        elif msg.upper() == "EXITING CHATROOM":
-            break
 
 
