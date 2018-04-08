18280eaccad031754ec7073b033f89b96306d65f
paulknisely <p.d.knisely@gmail.com>
2018-04-08 16:34:46 -0500

    client.py

--- REGISTRATION_SERVER.py
+++ REGISTRATION_SERVER.py
@@ -7,9 +7,9 @@ from socket import *
 from datetime import datetime
 
 #Create a socket bound at SERVER_PORT
-SERVER_PORT = 12015
+SERVER_PORT = 12116
 SERVER_SOCKET = socket(AF_INET, SOCK_STREAM)
-SERVER_SOCKET.bind(('', SERVER_PORT))
+SERVER_SOCKET.bind(('172.22.203.225', SERVER_PORT))
 SERVER_SOCKET.listen(10)
 print("Registration server is ready to receive")
 ACCESS_TIME = datetime.now()
@@ -19,62 +19,85 @@ while 1:
     CONNECTION_SOCKET, ADDR = SERVER_SOCKET.accept()
     print("from", ADDR)
     REGISTRATION_STATUS = "Failure"
+    LOGIN_STATUS = ""
     while 1:
-        REQUEST = CONNECTION_SOCKET.recv(1024).decode('ascii')
-        print("Request message: ", REQUEST)
-        if REQUEST.upper() == "QUIT":
-            break
-        METHOD_NAME = REQUEST.split("\t")[0].strip()
-        print("From ", ADDR, METHOD_NAME)
-        if METHOD_NAME.upper() == "REGISTER":
-            #Checking if userID has been registered
-            print("Checking if user has been registered...")
-            USER_ID = REQUEST.split("\t")[1]
-            USER_NAME = REQUEST.split("\t")[2]
-            USER_EMAIL = REQUEST.split("\t")[3]
-            USER_PASSWORD = REQUEST.split("\t")[4]
-            print(METHOD_NAME, USER_ID)
-            #Open the UserProfile.txt file to check the userID
+        RESPONSE = CONNECTION_SOCKET.recv(1024).decode('ascii')
+        print("Response message: ", RESPONSE)
+        if RESPONSE.upper() == "HELLO":
+            CONNECTION_SOCKET.send("Input your choice:\n\tRegister\n\tLogin\n\tQuit\n".encode())
+            RESPONSE = CONNECTION_SOCKET.recv(1024).decode('ascii')
+        if RESPONSE.upper() == "LOGIN":
+            CONNECTION_SOCKET.send("Enter username: ".encode())
+            USERNAME = CONNECTION_SOCKET.recv(1024).decode('ascii')
+            CONNECTION_SOCKET.send("Enter password: ".encode())
+            PASSWORD = CONNECTION_SOCKET.recv(1024).decode('ascii')
+            #Open the registeredusers.txt file to check the userID
             try:
-                print("Reading from UserProfile.txt")
-                for line in open("UserProfile.txt", "r"):
-                    REASON = ""
-                    if USER_ID not in line:
-                        if USER_NAME not in line and USER_EMAIL not in line:
-                            if len(USER_PASSWORD) >= 6:
-                                REGISTRATION_STATUS = "Success"
-                            else:
-                                print("Password length is shorter than 6")
-                                REASON += "Password length is shorter than 6\t"
-                                REGISTRATION_STATUS = "Failure"
-                        else:
-                            print("There is an existing user id for this user")
-                            REASON += "There is an existing user id for this user\t"
-                            REGISTRATION_STATUS = "Failure"
+                print("Reading from registeredusers.txt")
+                REASON = ""
+                for line in open("registeredusers.txt", "r"):
+                    if USERNAME not in line and PASSWORD not in line:
+                        LOGIN_STATUS = "Success"
                     else:
-                        print("The user id already exists")
-                        REASON += "The user id already exists\t"
-                        REGISTRATION_STATUS = "Failure"
-                    print("Done reading from UserProfile.txt")
+                        print("The username and password you entered are incorrect")
+                        REASON += "The username and password you entered are incorrect\n"
+                        LOGIN_STATUS = "Failure"
+                        break
+                    print("Done reading from registeredusers.txt")
             except FileNotFoundError:
                 print("File Not Found")
-            if REGISTRATION_STATUS == "Success":
+        #Start Registration code
+        if RESPONSE.upper() == "REGISTER":
+            CONNECTION_SOCKET.send("Enter email: ".encode())
+            EMAIL = CONNECTION_SOCKET.recv(1024).decode('ascii')
+            CONNECTION_SOCKET.send("Enter username: ".encode())
+            USERNAME = CONNECTION_SOCKET.recv(1024).decode('ascii')
+            CONNECTION_SOCKET.send("Enter password: ".encode())
+            PASSWORD = CONNECTION_SOCKET.recv(1024).decode('ascii')
+            #Open the registeredusers.txt file to check the userID
+            try:
+                print("Reading from registeredusers.txt")
+                REASON = ""
+                for line in open("registeredusers.txt", "r"):
+                    if USERNAME not in line:
+                        if USERNAME not in line and EMAIL not in line:
+                            if len(PASSWORD) >= 6:
+                                REGISTRATION_STATUS = "NEEDS_TO_REGISTER"
+                            else:
+                                print("Password length is shorter than 6")
+                                REASON += "Password length is shorter than 6\n"
+                                REGISTRATION_STATUS = "Failure"
+                                break
+                        else:
+                            print("The email you entered is already in use")
+                            REASON += "The email you entered is already in use\n"
+                            REGISTRATION_STATUS = "Failure"
+                            break
+                    else:
+                        print("The username you entered is already in use")
+                        REASON += "The username  you entered is already in use\n"
+                        REGISTRATION_STATUS = "Failure"
+                        break
+                    print("Done reading from registeredusers.txt")
+            except FileNotFoundError:
+                print("File Not Found")
+            if REGISTRATION_STATUS.upper() == "NEEDS_TO_REGISTER":
                 ACCESS_TIME = datetime.now()
                 STRING_ACCESS_TIME = ACCESS_TIME.strftime('%m/%d/%Y %H:%M:%S')
                 print("Access time is ", STRING_ACCESS_TIME)
-                REGISTRATION_RECORD = USER_ID+"\t"+USER_NAME+"\t"+USER_EMAIL+"\t"
-                REGISTRATION_RECORD += USER_PASSWORD+"\t"+ADDR[0]+"\t"+STRING_ACCESS_TIME+"\n"
-                OUTPUT_FILE = open("UserProfile.txt", "a")
+                REGISTRATION_RECORD = EMAIL+"\t"+USERNAME+"\t"+PASSWORD+"\t\n"
+                #REGISTRATION_RECORD += USER_PASSWORD+"\t"+ADDR[0]+"\t"+STRING_ACCESS_TIME+"\n"
+                OUTPUT_FILE = open("registeredusers.txt", "a")
                 OUTPUT_FILE.write(REGISTRATION_RECORD)
                 OUTPUT_FILE.close()
-            #Send the registration status response back to the client
-            RESPONSE = "Registration status: "+REGISTRATION_STATUS+"\r\n"
-            if REGISTRATION_STATUS == "Success":
-                CONNECTION_SOCKET.send(RESPONSE.encode())
-            else:
-                RESPONSE += REASON
-                CONNECTION_SOCKET.send(RESPONSE.encode())
-            print("Return to client: ", RESPONSE)
-        else:
-            break
+                SEND = "Registration Status: SUCCESS\n"
+                SEND += "Input your choice:\n\tRegister\n\tLogin\n\tQuit\n"
+                CONNECTION_SOCKET.send(SEND.encode())
+            if REGISTRATION_STATUS.upper() == "FAILURE":
+                SEND = "Registration Status: FAILED\n"
+                SEND += REASON
+                SEND += "Input your choice:\n\tRegister\n\tLogin\n\tQuit\n"
+                CONNECTION_SOCKET.send(SEND.encode())
+            if LOGIN_STATUS.upper() == "SUCCESS":
+                CONNECTION_SOCKET.send("Welcome to PodChat!".encode())
     CONNECTION_SOCKET.close()
